b58aa12ea72b53903ee557cf5397c834094ab6ad
Farhan Girach <farhangirach@hotmail.com>
2024-07-05 07:26:04 +0530

    Search functionality implimentation

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import './Table.scss'
+import React, { useState } from "react";
+import "./Table.scss";
 export interface Props {
   cols: Array<any>;
   data: Array<any>;
@@ -7,33 +7,78 @@ export interface Props {
   hoverable?: boolean;
   striped?: boolean;
   isDark?: boolean;
+  searchable?: boolean;
 }
-const Table = ({ cols, data, bordered, hoverable, striped, isDark }: Props) => {
+const Table = ({
+  cols,
+  data,
+  bordered,
+  hoverable,
+  striped,
+  isDark,
+  searchable,
+}: Props) => {
+  const [search, setSearch] = useState<any[]>(data);
+  const hadleSearch = (e: any) => {
+    const val = e.target.value;
+    if (val) {
+      setTimeout(() => {
+        setSearch([
+          ...data.filter((row: any) => {
+            return Object.values(row).join("").toLowerCase().includes(val);
+          }),
+        ]);
+      }, 2000);
+    } else {
+      setTimeout(() => {
+        setSearch(data);
+      }, 2000);
+    }
+  };
   return (
     <div className="tableWrap">
+      {searchable && (
+        <div className="tableOptions">
+          <input type="search" placeholder="Search..." onChange={hadleSearch} />
+        </div>
+      )}
+
       <table
-        className={`table
-        ${bordered ? " " + "table-bordered" : ""}
-        ${hoverable ? " " + "table-hover" : ""}
-        ${striped ? " " + "table-striped" : ""}
-        ${isDark ? " " + "table-dark" : ""}
-        `}
+        className={`table ${bordered ? " " + "table-bordered" : ""} ${
+          hoverable ? " " + "table-hover" : ""
+        } ${striped ? " " + "table-striped" : ""} ${
+          isDark ? " " + "table-dark" : ""
+        }`}
       >
         <thead>
           <tr>
-            {cols?.map((headerItem:any, index:any) => (
-              <th key={index} data-title={headerItem.title}>{headerItem.title}</th>
+            {cols?.map((headerItem: any, index: any) => (
+              <th key={index} data-title={headerItem.title}>
+                {headerItem.title}
+              </th>
             ))}
           </tr>
         </thead>
         <tbody>
-          {data?.map((item:any, index:any) => (
-            <tr key={index}>
-              {cols.map((col:any, key:any) => (
-                <td data-title={col.title} key={key}>{col.render(item)}</td>
+          {search.length !== 0
+            ? search?.map((item: any, index: any) => (
+                <tr key={index}>
+                  {cols.map((col: any, key: any) => (
+                    <td data-title={col.title} key={key}>
+                      {col.render(item)}
+                    </td>
+                  ))}
+                </tr>
+              ))
+            : data?.map((item: any, index: any) => (
+                <tr key={index}>
+                  {cols.map((col: any, key: any) => (
+                    <td data-title={col.title} key={key}>
+                      {col.render(item)}
+                    </td>
+                  ))}
+                </tr>
               ))}
-            </tr>
-          ))}
         </tbody>
       </table>
     </div>
