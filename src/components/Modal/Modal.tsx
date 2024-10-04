1739e27cf1bbfd474bcbf4b21b7feed9be387bad
Farhan Girach <farhangirach@hotmail.com>
2024-10-04 12:10:25 +0530

    close modal on esc key press

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 import './Modal.scss';
 export type width = 'small' | 'medium' | 'large';
 export interface Props {
@@ -16,15 +16,31 @@ const Modal = ({
   headerText
 }: Props) => {
   const [modal, setModal] = useState(false);
-  toggle = () => setModal(!modal)
+  const modalRef = useRef<any>(null);
+  const modalOpen = () => {
+    setModal(true);
+  }
+  const modalClose = () => {
+    setModal(false);
+    modalRef?.current?.focus();
+  }
+  useEffect(() => {
+    document.addEventListener('keydown', (e) => {
+      if (e.key === 'Escape') {
+        setModal(false);
+        modalRef?.current?.focus();
+      }
+    });
+   
+  },[]);
   return (
     <>
-      <button onClick={toggle}>{buttonText}</button>
+      <button ref={ modalRef } onClick={modalOpen}>{buttonText}</button>
       {
         modal &&
         <div className={`modal ${width}`}>
             <div className='modalContent'>
-              <button className='close' onClick={toggle}>X</button>
+              <button className='close' onClick={modalClose}>X</button>
               {
                 headerText && <div className='modelHeader'>{headerText}</div>
               }
